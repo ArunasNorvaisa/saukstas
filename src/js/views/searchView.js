65eb2e22c558e2f25f33b83fc3352784f5c248a0
Arūnas Norvaiša <an@86.lt>
2019-03-24 13:12:23 +0200

    recipe title length limited to one line

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,6 +8,21 @@ export const clearPreviousResults = () => {
     elements.searchResList.innerHTML = '';
 };
 
+const limitRecipeTitleLength = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if(acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+        console.log('newTitle:', newTitle)
+        return `${newTitle.join(' ')}...`;
+    }
+    return title;
+};
+
 const renderRecipe = recipe => {
     const html = `
         <li>
@@ -16,7 +31,7 @@ const renderRecipe = recipe => {
                     <img src="${recipe.image_url}" alt="${recipe.title}">
                 </figure>
                 <div class="results__data">
-                    <h4 class="results__name">${recipe.title}</h4>
+                    <h4 class="results__name">${limitRecipeTitleLength(recipe.title)}</h4>
                     <p class="results__author">${recipe.publisher}</p>
                 </div>
             </a>
