1f4a80eb80ca7a5eec93a85982ed45d17b796f72
Arūnas Norvaiša <an@86.lt>
2019-03-24 12:29:29 +0200

    search results rendered to the UI

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,36 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-const search = new Search('beef steak');
-console.log(search);
-search.getResults();
+/**
+ * Global state of the app:
+ * - Search object;
+ * - Current recipe object;
+ * - Shopping list object;
+ * - Liked recipes.
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1. Get the query from the view
+    const query = searchView.getInput();
+
+    if(query) {
+        // 2. Create new search object and add it to state
+        state.search = new Search(query);
+        // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearPreviousResults();
+        // 4. Search for recipes
+        await state.search.getResults();
+        // 5. Render results to UI
+        console.log(state.search.result);
+        searchView.renderResults(state.search.result);
+
+    }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -4,7 +4,7 @@ export default class Search {
     constructor(query) {
         this.query = query;
     }
-    
+
     async getResults() {
 /*        const key = '80446f14fc7beddbcd9232a8a883e6b3';
         const url = `https://cors-anywhere.herokuapp.com
@@ -13,9 +13,7 @@ export default class Search {
         const url = 'https://raw.githubusercontent.com/ArunasNorvaisa/saukstas/master/db.json'
         try {
             const res = await axios(url);
-            console.log('res', res);
             this.result = res.data.recipes;
-            console.log(this.result);
         } catch(error) {
             alert(error);
         };
