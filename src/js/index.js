00306aec85ed6abcbe38aed46b5fa61deedec39f
Arunas Norvaisa <an@86.lt>
2019-03-31 08:18:01 +0300

    Likes controller semi-finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
+import Likes from './models/Likes';
 import * as searchView from "./views/searchView";
 import * as recipeView from './views/recipeView';
 import * as listView from './views/listView';
+//import * as likesView from './views/likesView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /**
@@ -122,11 +124,14 @@ elements.recipe.addEventListener('click', e => {
     } else if(e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
         //Now, we handle adding to favourites
         controlList();
+    } else if(e.target.matches('.recipe__love, .recipe__love *')) {
+        //Handle LIKE button click
+        controlLike();
     }
 });
 
 /**
-* FOVORITE LIST CONTROLLER
+* SHOPPING LIST CONTROLLER
 */
 const controlList = () => {
     //Create new list, if there is none yet
@@ -155,3 +160,31 @@ elements.shopping.addEventListener('click', e => {
     }
     
 });
+
+/**
+* LIKES CONTROLLER
+*/
+const controlLike = () => {
+    if(!state.likes) state.likes = new Likes();
+    const currentID = state.recipe.id;
+    if(!state.likes.isLiked(currentID)) {
+        //Add like to the state
+        const newLike = state.likes.addLike(
+            currentID,
+            state.recipe.author,
+            state.recipe.title,
+            state.recipe.img
+        );
+        
+        //Toggle the LIKE button view
+        
+        //Add like to the UI        
+    } else {
+        //Remove like from the state
+        state.likes.deleteLike(currentID);
+        
+        //Toggle the LIKE button view
+        
+        //Remove like from the UI
+    }
+};
